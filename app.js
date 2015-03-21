03ad37edb44b4d609de6fb6d4d32fe404d4229aa
Ismael Slimane <isma.bled777@gmail.com>
2015-03-21 12:15:19 +0100

    Added users images dynamically with ajax

--- app.js
+++ app.js
@@ -2,30 +2,33 @@ $(document).ready(function() {
 	$.getJSON("timeline.json", function(data) {
 		for(var i=0;i<data.length;i+=1){
 			msg = '<div class="msg">';
-			msg += ' <img class="' + data[i].Autor + '" height="42" width="42"> '
-			msg += '<spam class="date">' + data[i].Fecha + '</spam>'
-			msg += '<p class="text">@' + data[i].Autor + ': ' + data[i].Titulo + '<p>'
-			msg += '<p class="text">' + data[i].Contenido + '<p>'
-			msg += '</div>'
+			msg += '<spam class="date">' + data[i].Fecha + '</spam>';
+			msg += '<p class="text">';
+			msg += '<img class="' + data[i].Autor + '" height="42" width="42">@';
+			msg += data[i].Autor + ': ' + data[i].Titulo + '<p>';
+			msg += '<p class="text">' + data[i].Contenido + '<p>';
+			msg += '</div>';
 			$("#log").append(msg);
-			setImg(data[i].Autor);
 		}
 	});
 
 	$.getJSON("myline.json", function(data) {
 		for(var i=0;i<data.length;i+=1){
 			msg = '<div class="msg">';
-			msg += '<spam class="date">' + data[i].Fecha + '</spam>'
-			msg += '<p class="text">@' + data[i].Autor + ': ' + data[i].Titulo + '<p>'
-			msg += '<p class="text">' + data[i].Contenido + '<p>'
-			msg += '</div>'
+			msg += '<spam class="date">' + data[i].Fecha + '</spam>';
+			msg += '<p class="text">';
+			msg += '<img class="' + data[i].Autor + '" height="42" width="42">@';
+			msg += data[i].Autor + ': ' + data[i].Titulo + '<p>';
+			msg += '<p class="text">' + data[i].Contenido + '<p>';
+			msg += '</div>';
 			$("#mylog").append(msg);
+			console.log("setImg: " + data[i].Autor);
 		}
 	});
 
 	var updateData;
 
-	$.getJSON("update.json", function(data) {
+	$.getJSON("update.json", function(data){
 		updateData = data;
 		if(updateData.length>0){
 			$('#btn').html('<button id="showMsgs">' + updateData.length + ' New Messages</button>');
@@ -36,22 +39,35 @@ $(document).ready(function() {
 		var msg = "";
 		for(var i=0;i<updateData.length;i+=1){
 			msg += '<div class="msg">';
-			msg += '<p class="text">@' + updateData[i].Autor + ': ' + updateData[i].Titulo + '<p>'
-			msg += '<p class="text">' + updateData[i].Contenido + '<p>'
+			msg += '<p class="text">';
+			msg += '<img class="' + updateData[i].Autor + '" height="42" width="42">@';
+			msg += updateData[i].Autor + ': ' + updateData[i].Titulo + '<p>';
+			msg += '<p class="text">' + updateData[i].Contenido + '<p>';
 			msg += '</div>'
 		}
 		$("#newMsgs").replaceWith(msg);
+		setUserImgs(users);
 	});
+
+	var users = ['subject1', 'subject2', 'subject3', 'subject4', 'islimane'];
+
+	var setUserImgs = function(users){
+		for(user in users){
+			setImg(users[user]);
+		}
+	}
 
 	var setImg = function(user){
 		$.ajax({
 			type:"GET",
-			dataType: "image/png",
+			contentType: "image/png",
+			cache: false,
 			url: "img/" + user + ".png",
-			success: function (img) {
-				console.log('.' + user);
-				$('.' + user).attr('src', img);
-			  }
+			success: function (img){
+				$('.' + user).attr('src', "img/" + user + ".png");
+			}
 		});
 	}
+
+	setUserImgs(users);
 });
